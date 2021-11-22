a0ba90a1d1c8e16cd0702a8fb978de0c7638974b
wangyiben28155 <wangyiben28155@gmail.com>
2021-11-22 21:11:06 +0800

    next

--- Numerical_Diff_DiscreteFuc.jl
+++ Numerical_Diff_DiscreteFuc.jl
@@ -4,31 +4,43 @@ export Three_Point, Five_Point
 
 using SparseArrays
 
-macro Dimension_Judge(x::Vector{Any}, y::Vector{Any})
-    quote
-        if length($x) != length($y)
-            @error "The dimension of x and y is not equal." x_length = length(x) y_length = length(y)
-        end
+function Dimension_Judge(x::Vector{<:Any}, y::Vector{<:Any})
+    if length(x) != length(y)
+        @error "The dimension of x and y is not equal." x_length = length(x) y_length = length(y)
+    elseif length(x) <= 2
+        @error "The length is too short."
     end
 end
 
 function Three_Point(x::Vector{T}, y::Vector{T}; dL = Parameter.dx) where {T<:AbstractFloat}           #在程序中我们的空间步长都是一致的, 我们只需要计算
     local coefficient = 1 / (2 * dL)
-    local derivative_numrical_func = zeros(length(x))
-    local Transform_Matrix = sparse(1)
+    local num = length(x)
+    local derivative_numrical_func = zeros(num)
+    local Transform_Matrix::SparseMatrixCSC
+    local Section_1 = [-3.0, 4.0, -1.0]
+    local Section_2 = [-1.0, 1.0]
+    local I, J, V = (zeros(2 * (num + 1)), 
+                     zeros(2 * (num + 1)), 
+                     zeros(2 * (num + 1)))
+
+    V = [Section_1;repeat(Section_2, outer = 2 * (n-2));-reverse(Section_1)]
+    I = [[1,1,1];repeat(collect(2:num-1), inner = 2);[num, num, num]]
+    J = [[1,2,3];]
 
 
-    return x, derivative_numrical_func
+
+
+
+
+    derivative_numrical_func = Transform_Matrix * y
+
+    return x, coefficient .* derivative_numrical_func
 end
 
 function Five_Point(x::Vector{T}, y::Vector{T}; dL = Parameter.dx) where {T<:AbstractFloat}
     local coefficient = 1 / (2 * dL)
     local derivative_numrical_func = zeros(length(x))
     local Transform_Matrix = sparse()
-
-    if length(x) != length(y)
-        @error "The dimension of x and y is not equal."
-    end
 
 
 
