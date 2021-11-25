893f4b7cc521589991fb5ba7011a093e86542884
wangyiben28155 <wangyiben28155@gmail.com>
2021-11-25 17:02:21 +0800

    change

--- Main_module.jl
+++ Main_module.jl
@@ -1,24 +1,29 @@
 module TDQMC
 
-export Three
+export Dynamics, Parameter
 
-using Distributions
+using Distributions                   #用来初始化初始的波函数和系综粒子分布
 import Base.@kwdef
 
-const L, x_num, step_t = (350.0, 1751, 13001)
+const Ensemble_num, L, x_num, step_t = (50, 350.0, 1751, 13001)
 
 
 @kwdef mutable struct Dynamics{T<:AbstractFloat}
-    real_space::Vector{Complex{T}} = complex(collect(pdf(Truncated(Normal(0, 30), -Inf64, Inf64), LinRange(-L, L, x_num))))
+    Guide_Wave::Vector{Complex{T}} = complex(collect(pdf(Truncated(Normal(0, 30), -Inf64, Inf64), LinRange(-L, L, x_num))))
     Time::Union{T,Complex{T}} = 0.0
-end
-
-
-@kwdef struct Parameter{T<:AbstractFloat}
-    
 
 end
- 
+
+
+ @kwdef struct Parameter{T<: AbstractFloat}              #用来控制计算参数的
+        N::Int64 = x_num                                    #划分的格点的总数,后面做离散傅里叶变换的时候会用得到
+        scope::T = L                                        #确定波函数的计算范围为-scope到+scope
+        Δx::T = 2 * scope/(N-1)                             #波函数的离散的空间间隔
+        Δt::Union{T,Complex{T}} =  0.05                     #划分的时间间隔, 尝试时间迭代区间, 因为考虑到虚时演化, 所以类型设定为复数
+        Step_t::Int64 = step_t    
+        sampling :: Vector{T} = collect(LinRange(-scope, scope, N))
+end    
+
 include("Numerical_Diff_DiscreteFunc.jl")      #用来做数值微分的函数, 对离散函数(不知道解析表达式的波函数进行求解)
 include("Potential.jl")
 # include("")
--- Numerical_Diff_DiscreteFunc.jl
+++ Numerical_Diff_DiscreteFunc.jl
@@ -1,7 +1,8 @@
 module Discrete_Func_diff
 
-export Three_Point, Five_Point
+export Three_Point, Five_Point, Derivative_2
 
+#using ..TDQMC
 using SparseArrays
 
 function Dimension_Judge(x::AbstractArray, y::AbstractArray)
@@ -12,16 +13,16 @@ function Dimension_Judge(x::AbstractArray, y::AbstractArray)
     end
 end
 
-function Three_Point(x::Vector{T}, y::Vector{T}; dL = Parameter.dx) where {T<:AbstractFloat}   #在程序中我们的空间步长都是一致的, 我们只需要计算
+function Three_Point(x::Vector{T}, y::Vector{<:Union{Complex{T},T}}; dL = Parameter.Δx) where {T<:AbstractFloat}   #在程序中我们的空间步长都是一致的
     local coefficient = 1 / (2 * dL)
     local num = length(x)
-    local derivative_numrical_func = zeros(T, num)
-    local Transform_Matrix::SparseMatrixCSC
+    local derivative_numrical_func = zeros(eltype(y), num)
+    local Transform_Matrix::SparseMatrixCSC = spzeros(T, num, num)
     local Section_1 = [-3.0, 4.0, -1.0]
     local Section_2 = [-1.0, 1.0]
     local I, J, V = (zeros(2 * (num + 1)),
         zeros(2 * (num + 1)),
-        zeros(2 * (num + 1)))
+        zeros(T, 2 * (num + 1)))
 
     V = [Section_1; repeat(Section_2, outer = num - 2); -reverse(Section_1)]
     I = [[1, 1, 1]; repeat(collect(2:num-1), inner = 2); [num, num, num]]
@@ -33,16 +34,16 @@ function Three_Point(x::Vector{T}, y::Vector{T}; dL = Parameter.dx) where {T<:Ab
     return x, derivative_numrical_func
 end
 
-function Five_Point(x::Vector{T}, y::Vector{T}; dL = Parameter.dx) where {T<:AbstractFloat}
+function Five_Point(x::Vector{T}, y::Vector{<:Union{Complex{T},T}}; dL = Parameter.Δx) where {T<:AbstractFloat}        #五步法计算一阶导数
     local coefficient = 1 / (12 * dL)
     local num = length(x)
-    local derivative_numrical_func = zeros(Float64, num)
-    local Transform_Matrix::SparseMatrixCSC
+    local derivative_numrical_func = zeros(eltype(y), num)
+    local Transform_Matrix::SparseMatrixCSC = spzeros(T, num, num)
     local Section_1 = [-25.0, 48.0, -36.0, 16.0, -3.0]
     local Section_2 = [1.0, -8.0, 8.0, -1.0]
     local I, J, V = (zeros(4 * (num + 1)),
         zeros(4 * (num + 1)),
-        zeros(4 * (num + 1)))
+        zeros(T, 4 * (num + 1)))
 
     V = [repeat(Section_1, outer = 2); repeat(Section_2, outer = num - 4); repeat(-reverse(Section_1), outer = 2)]
     I = [repeat([1, 2], inner = 5); repeat(collect(3:num-2), inner = 4); repeat([num - 1, num], inner = 5)]
@@ -53,6 +54,30 @@ function Five_Point(x::Vector{T}, y::Vector{T}; dL = Parameter.dx) where {T<:Abs
     derivative_numrical_func = coefficient .* (Transform_Matrix * y)
 
     return x, derivative_numrical_func
+end
+
+
+function Derivative_2(x::Vector{T}, y::Vector{<:Union{Complex{T},T}}; dL = Parameter.Δx) where {T<:AbstractFloat}
+    local coefficient = 1 / (12 * dL^2)
+    local num = length(x)
+    local derivative_two_func = zeros(eltype(y), num)
+    local Transform_Matrix::SparseMatrixCSC = spzeros(T, num, num)
+    local Section_1 = [35.0, -104.0, 114.0, -56.0, 11.0]
+    local Section_2 = [11.0, -20.0, 6.0, 4.0, -1.0]
+    local Section_3 = [-1.0, 16.0, -30.0, 16.0, -1.0]
+    local I, J, V = (zeros(5 * num),
+        zeros(5 * num),
+        zeros(T, 5 * num))
+
+    V = [Section_1; Section_2; repeat(Section_3, outer = num - 4); reverse(Section_2); reverse(Section_1)]
+    I = repeat(collect(1:num), inner = 5)
+    J = [repeat(collect(1:5), outer = 2); vcat([collect(1:5) .+ i for i = 0:num-5]...); repeat(collect(num-4:num), outer = 2)]
+
+    Transform_Matrix = sparse(I, J, V)
+
+    derivative_two_func = coefficient .* (Transform_Matrix * y)
+
+    return x, derivative_two_func
 end
 
 end
