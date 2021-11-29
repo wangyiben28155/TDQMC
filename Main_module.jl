f12462f1002fb1d05f249f408877aa5b903e31b6
wangyiben28155 <wangyiben28155@gmail.com>
2021-11-29 16:08:52 +0800

    change

--- Main_module.jl
+++ Main_module.jl
@@ -1,6 +1,6 @@
 module TDQMC
 
-export Dynamics, Parameter, generate_distribution
+export Dynamics, Parameter, generate_distribution, Initializer_GuideWave
 
 using Distributions, Random                                     #用来初始化初始的波函数和系综粒子分布
 import Base.@kwdef
@@ -10,21 +10,33 @@ const L, x_num, step_t = (350.0, 1751, 13001)
 const μ, σ = (0, 30)
 
 
+function Initializer_GuideWave()                                #这里因为算的是一维的波函数, 所以返回的矩阵为三维矩阵,第一维为波函数,第二维为系综粒子数,第三维为电子数
+    local A = complex(sqrt.(pdf(Normal(μ, σ), LinRange(-L, L, x_num))))
+    local B:: Array{eltype(A),3} = zeros(eltype(A), (length(A), Ensemble_num, Electron_num))
+
+    for i in 1:Ensemble_num, j in 1:Electron_num
+        @inbounds B[:,i,j] = A
+    end
+
+    return B
+end
+
 function generate_distribution(x::Int)
     Random.seed!(x)                                             #确定下一次使用rand函数的撒点是固定的, 每次调用程序也能保证下面的分布是确定的
     Initial_Distriution = rand(Normal(μ, σ), Ensemble_num)      #将确定的撒点分布储存起来, 用于下面实例化的赋值
 
-    return Initial_Distriution
+    return Initial_Distriution'
 end
 
 @kwdef mutable struct Dynamics{T<:AbstractFloat}
-    Guide_Wave::Vector{Complex{T}} = complex(sqrt.(pdf(Normal(μ, σ), LinRange(-L, L, x_num))))#这里得到的分布是概率密度的开平方作为初始的波函数进行演化
-    Trajectory::Matrix{T} = hcat(generate_distribution.(1:Electron_num))            #先给定一个初始的轨迹分布,目前只是计算氢原子的情况,所以先只使用向量保存当前时刻的位置信息
+    Guide_Wave::Array{Complex{T},3} = Initializer_GuideWave()                          #这里得到的分布是概率密度的开平方作为初始的波函数进行演化
+    Trajectory::Matrix{T} = vcat(generate_distribution.(1:Electron_num)...)            #先给定一个初始的轨迹分布,目前只是计算氢原子的情况,所以先只使用向量保存当前时刻的位置信息, 并且之后代表不同电子之间粒子的位置之间的运算会有矢量化运算,所以将其放在列坐标加快运算
     Time::Union{T,Complex{T}} = 0.0
 end
 
 
 @kwdef struct Parameter{T<:AbstractFloat}                        #用来控制计算参数的
+    particle::Int64 = Ensemble_num
     space_N::Int64 = x_num                                       #划分的格点的总数,后面做离散傅里叶变换的时候会用得到
     scope::T = L                                                 #确定波函数的计算范围为-scope到+scope
     Δx::T = 2 * scope / (N - 1)                                  #波函数的离散的空间间隔
--- Physical_quantity.jl
+++ Physical_quantity.jl
@@ -6,7 +6,8 @@ using ..TDQMC
 using ..TDQMC.Discrete_Func_diff
 
 function Energy(P::Parameter, Dy::Dynamics)
-    
+    local coefficient = 1/P.particle
+    local A
 
 
 
